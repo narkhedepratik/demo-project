f9af96b1862e0f101e694ec0374239c2429e0433
GayatriGadbail <gdgadbail2003@gmail.com>
2024-08-14 15:10:53 +0530

    header by gayatri

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import Header from './template/header/Header.js'
 function App() {
   return (
     <div className="App">
       <h1>Demo Project for git</h1>
+      <Header/>
     </div>
   );
 }
