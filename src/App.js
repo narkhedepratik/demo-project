48212a82aa39b7f223a4cd4f7d04472775b8b272
GayatriGadbail <gdgadbail2003@gmail.com>
2024-08-16 17:56:28 +0530

    footer components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Footer from './template/header/footer/Footer.js';
 import Header from './template/header/Header.js'
 function App() {
   return (
     <div className="App">
       <h1>Demo Project for git</h1>
       <Header/>
+      <Footer/>
     </div>
   );
 }
